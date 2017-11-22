4af70b75376b226f6ddcbb164ee0fbb48a3d4ac6
Kyle Higginson <kylehigginson11@hotmail.com>
2017-11-22 23:18:21 +0000

    amended see more feature

--- scripts/article_section.js
+++ scripts/article_section.js
@@ -1,7 +1,21 @@
 // Wait for DOM objects to be loaded, then load popular news stories
 $(document).ready(function () {
-    $('section button.see-more-btn').click(function () {
-        var section = $('#sportNewsSection .extra');
+    /*
+     This controls the buttons in the navigation bar, by selecting the button in the header
+     which are siblings of the tab class. Again, using the data-target value, scroll to the div with ID matching
+     data-target.
+     */
+    $("header nav li a").click(function () {
+        var section = $("#" + $(this).data('target'));
+        $('html, body').animate({
+            scrollTop: section.offset().top
+        }, 2000);
+    });
+
+    // show extra articles for recent and popular articles
+    $(document).on("click", "header button.see-more-btn", function () {
+        var target = "#" + $(this).data("target");
+        var section = $(target + '.row .extra');
         if ($(section).css('display') === "none") {
             $(section).show("slide", {direction: "left"}, 1000);
             $(this).text("See Less");
@@ -11,12 +25,4 @@ $(document).ready(function () {
         }
     });
 
-    $(document).on("click", "button.full-article", function () {
-        var index = $(this).data("target");
-        var category = $(this).attr('data-category');
-        $('#articleTitle').text(newsData[category]["articles"]["results"][index].title);
-        $('#articleBody').text(newsData[category]["articles"]["results"][index].body);
-        $('#articleImg').attr('src', newsData[category]["articles"]["results"][index].image);
-        $('#seeMoreModal').modal('show');
-    });
 });
--- scripts/news_site.js
+++ scripts/news_site.js
@@ -1,17 +1,24 @@
 // Wait for DOM objects to be loaded, then load popular news stories
 $(document).ready(function () {
+
     var newsData = new Object();
     var searchData = [];
+
     $.get("sample_data/popular_articles.json", function (data, status) {
         newsData["popular"] = data;
         for (var i = 0; i < 4; i++) {
-            $('#popularHeader').append('<div class="col-md-3"><h6>' + data.articles.results[i]['title'] + '</h6>' + '<img src="' + data.articles.results[i]['image'] +
+            $('#popularHeader').prepend('<div class="col-md-3"><h6>' + data.articles.results[i]['title'] + '</h6>' + '<img src="' + data.articles.results[i]['image'] +
+                '"><button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="popular">Full Article</button></div>');
+        }
+
+        for (var x = 5; x < 9; x++) {
+            $('#popularHeader > .extra .row').append('<div class="col-md-3"  data-target="' + x + '"><h6>' + data.articles.results[x]['title'] + '</h6>' + '<img src="' + data.articles.results[x]['image'] +
                 '"><button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="popular">Full Article</button></div>');
         }
 
         for (var a = 0; a < data.articles.results.length; a++) {
             searchData.push({
-                "title": data.articles.results[a]["title"],
+                "label": data.articles.results[a]["title"],
                 "category": "popular",
                 "index": a
             });
@@ -21,19 +28,38 @@ $(document).ready(function () {
     $.get("sample_data/recent_articles.json", function (data, status) {
         newsData["recent"] = data;
         for (var i = 0; i < 4; i++) {
-            $('#recentHeader').append('<div class="col-md-3"><h6>' + data.articles.results[i]['title'] + '</h6>' + '<img src="' + data.articles.results[i]['image'] + '"><button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="recent">Full Article</button></div>');
+            $('#recentHeader').prepend('<div class="col-md-3"><h6>' + data.articles.results[i]['title'] + '</h6>' + '<img src="' + data.articles.results[i]['image'] + '"><button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="recent">Full Article</button></div>');
+        }
+
+        for (var x = 5; x < 9; x++) {
+            $('#recentHeader > .extra .row').append('<div class="col-md-3"  data-target="' + x + '"><h6>' + data.articles.results[x]['title'] + '</h6>' + '<img src="' + data.articles.results[x]['image'] +
+                '"><button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="popular">Full Article</button></div>');
+        }
+
+        for (var a = 0; a < data.articles.results.length; a++) {
+            searchData.push({
+                "label": data.articles.results[a]["title"],
+                "category": "recent",
+                "index": a
+            });
         }
     });
 
     $.get("sample_data/sport_articles.json", function (data, status) {
-        newsData["sports"] = data;
+        newsData["sport"] = data;
         for (var i = 0; i < 6; i++) {
             $('#sportNewsSection > .container > .row:first').append('<div class="col-md-2"><h6>' + data.articles.results[i]['title'] + '</h6>' + '<img src="' + data.articles.results[i]['image'] + '">' +
-                '<button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="sports">Full Article</button></div>');
+                '<button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="sport">Full Article</button></div>');
         }
-        for (var x = 7; x < 13; x++) {
-            $('#sportNewsSection > .container > .extra .row').append('<div class="col-md-2"  data-target="' + x + '"><h6>' + data.articles.results[x]['title'] + '</h6>' + '<img src="' + data.articles.results[x]['image'] + '"></div>');
+
+        for (var a = 0; a < data.articles.results.length; a++) {
+            searchData.push({
+                "label": data.articles.results[a]["title"],
+                "category": "sport",
+                "index": a
+            });
         }
+
     });
 
     $(function () {
@@ -41,22 +67,34 @@ $(document).ready(function () {
             minLength: 0,
             source: searchData,
             focus: function (event, ui) {
-                $("#input").val(ui.item.title);
                 return false;
             },
             select: function (event, ui) {
-                $("#input").val(ui.item.title);
-                window.location.href = '/product/' + ui.item.id;
+                $("#input").val(ui.item.label);
+                var index = ui.item.index;
+                var category = ui.item.category;
+                $('#articleTitle').text(newsData[category]["articles"]["results"][index].title);
+                $('#articleBody').text(newsData[category]["articles"]["results"][index].body);
+                $('#articleImg').attr('src', newsData[category]["articles"]["results"][index].image);
+                $('#seeMoreModal').modal('show');
                 return false;
             }
         })
 
             .data("ui-autocomplete")._renderItem = function (ul, item) {
             return $("<li>")
-                .append("<a>" + item.title + "</a>")
+                .append("<a>" + item.label + "</a>")
                 .appendTo(ul);
         };
     });
 
+    $(document).on("click", "button.full-article", function () {
+        var index = $(this).data("target");
+        var category = $(this).attr('data-category');
+        $('#articleTitle').text(newsData[category]["articles"]["results"][index].title);
+        $('#articleBody').text(newsData[category]["articles"]["results"][index].body);
+        $('#articleImg').attr('src', newsData[category]["articles"]["results"][index].image);
+        $('#seeMoreModal').modal('show');
+    });
 
 });
