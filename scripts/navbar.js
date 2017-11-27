4da826b825cceb15762e4e4e98e57542bfdf6d26
Kyle Higginson <kylehigginson11@hotmail.com>
2017-11-27 19:55:15 +0000

    switched to guardian API

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,7 +1,13 @@
 // Wait for DOM objects to be loaded, then load popular news stories
 $(document).ready(function () {
 
+    //$('[data-toggle="popover"]').popover('toggle');
+
     $('#clearInputButton').click(function () {
         $('#input').val("");
+    });
+
+    $('nav p > a').click(function () {
+        $('[data-toggle="popover"]').popover('toggle');
     })
 });
--- scripts/news_site.js
+++ scripts/news_site.js
@@ -7,18 +7,35 @@ $(document).ready(function () {
     $.get("sample_data/popular_articles.json", function (data, status) {
         newsData["popular"] = data;
         for (var i = 0; i < 4; i++) {
-            $('#popularHeader').find('.row:first').prepend('<div class="col-md-3"><h6>' + data.articles.results[i]['title'] + '</h6>' + '<img src="' + data.articles.results[i]['image'] +
-                '"><button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="popular">View More</button></div>');
+            try {
+                $('#popularHeader').find('.row:first').prepend('<div class="col-md-3"><h6>' + data.response.mostViewed[i]['webTitle'] + '</h6>' + '<img src="' + data.response.mostViewed[i]['blocks']['body'][4]['elements'][1]['assets'][0]['file'] +
+                    '" class="img-fluid"><button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="popular">View More</button></div>');
+            }
+            catch (error) {
+                try {
+                    $('#popularHeader').find('.row:first').prepend('<div class="col-md-3"><h6>' + data.response.mostViewed[i]['webTitle'] + '</h6>' + '<img src="' + data.response.mostViewed[i]['blocks']['main']['elements'][0]['assets'][0]['file'] +
+                        '" class="img-fluid"><button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="popular">View More</button></div>');
+                } catch (error) {
+                    console.log("catch");
+                    $('#popularHeader').find('.row:first').prepend('<div class="col-md-3"><h6>' + data.response.mostViewed[i]['webTitle'] + '</h6>' + '<img src="images/image_unavailable.png" ' +
+                        'class="img-fluid"><button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="popular">View More</button></div>');
+                }
+            }
         }
 
         for (var x = 5; x < 9; x++) {
-            $('header').find('.extra .row').append('<div class="col-md-3"  data-target="' + x + '"><h6>' + data.articles.results[x]['title'] + '</h6>' + '<img src="' + data.articles.results[x]['image'] +
-                '"><button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="popular">View More</button></div>');
+            try {
+                $('header').find('.extra .row').append('<div class="col-md-3"  data-target="' + x + '"><h6>' + data.response.mostViewed[x]['webTitle'] + '</h6>' + '<img src="' + data.response.mostViewed[x]['blocks']['main']['elements'][0]['assets'][0]['file'] +
+                    '" class="img-fluid"><button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="popular">View More</button></div>');
+            } catch (error) {
+                $('header').find('.extra .row').append('<div class="col-md-3"  data-target="' + x + '"><h6>' + data.response.mostViewed[x]['webTitle'] + '</h6>' + '<img src="" ' +
+                    'class="img-fluid"><button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="popular">View More</button></div>');
+            }
         }
 
-        for (var a = 0; a < data.articles.results.length; a++) {
+        for (var a = 0; a < data.response.mostViewed.length; a++) {
             searchData.push({
-                "label": data.articles.results[a]["title"],
+                "label": data.response.mostViewed[a]["webTitle"],
                 "category": "popular",
                 "index": a
             });
@@ -28,8 +45,8 @@ $(document).ready(function () {
     $.get("sample_data/recent_articles.json", function (data, status) {
         var recentNews = "";
 
-        for (var a = 0; a < 10; a++) {
-            recentNews += data.articles.results[a]["title"] + " - RECENT NEWS - ";
+        for (var a = 0; a < 6; a++) {
+            recentNews += data.response.results[a]["webTitle"] + " - RECENT NEWS - ";
         }
 
         $('#recentNewsMarquee').text(recentNews);
@@ -38,13 +55,18 @@ $(document).ready(function () {
     $.get("sample_data/sport_articles.json", function (data, status) {
         newsData["sport"] = data;
         for (var i = 0; i < 6; i++) {
-            $('#sportNewsSection').find('.container > .row:first').append('<div class="col col-sm-4 col-lg-2"><h6>' + data.articles.results[i]['title'] + '</h6>' + '<img src="' + data.articles.results[i]['image'] + '">' +
-                '<button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="sport">View More</button></div>');
+            try {
+                $('#sportNewsSection').find('.container > .row:first').append('<div class="col col-sm-4 col-lg-2"><h6>' + data.response.results[i]['webTitle'] + '</h6>' + '<img src="' + data.response.results[i]['blocks']['main']['elements'][0]['assets'][0]['file'] + '" class="img-fluid">' +
+                    '<button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="sport">View More</button></div>');
+            } catch (error) {
+                $('#sportNewsSection').find('.container > .row:first').append('<div class="col col-sm-4 col-lg-2"><h6>' + data.response.results[i]['webTitle'] + '</h6>' + '<img src="images/image_unavailable.png" class="img-fluid">' +
+                    '<button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="sport">View More</button></div>');
+            }
         }
 
-        for (var a = 0; a < data.articles.results.length; a++) {
+        for (var a = 0; a < data.response.results.length; a++) {
             searchData.push({
-                "label": data.articles.results[a]["title"],
+                "label": data.response.results[a]["webTitle"],
                 "category": "sport",
                 "index": a
             });
@@ -54,13 +76,18 @@ $(document).ready(function () {
     $.get("sample_data/tech_articles.json", function (data, status) {
         newsData["tech"] = data;
         for (var i = 0; i < 6; i++) {
-            $('#techNewsSection').find('.container > .row:first').append('<div class="col-md-2"><h6>' + data.articles.results[i]['title'] + '</h6>' + '<img src="' + data.articles.results[i]['image'] + '">' +
-                '<button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="tech">View More</button></div>');
+            try {
+                $('#techNewsSection').find('.container > .row:first').append('<div class="col-md-2"><h6>' + data.response.results[i]['webTitle'] + '</h6>' + '<img src="' + data.response.results[i]['blocks']['main']['elements'][0]['assets'][0]['file'] + '" class="img-fluid">' +
+                    '<button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="tech">View More</button></div>');
+            } catch (error) {
+                $('#techNewsSection').find('.container > .row:first').append('<div class="col-md-2"><h6>' + data.response.results[i]['webTitle'] + '</h6>' + '<img src="images/image_unavailable.png" class="img-fluid">' +
+                    '<button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="tech">View More</button></div>');
+            }
         }
 
-        for (var a = 0; a < data.articles.results.length; a++) {
+        for (var a = 0; a < data.response.results.length; a++) {
             searchData.push({
-                "label": data.articles.results[a]["title"],
+                "label": data.response.results[a]["webTitle"],
                 "category": "tech",
                 "index": a
             });
@@ -70,13 +97,19 @@ $(document).ready(function () {
     $.get("sample_data/politics_articles.json", function (data, status) {
         newsData["politics"] = data;
         for (var i = 0; i < 6; i++) {
-            $('#politicsNewsSection').find('.container > .row:first').append('<div class="col-md-2"><h6>' + data.articles.results[i]['title'] + '</h6>' + '<img src="' + data.articles.results[i]['image'] + '">' +
-                '<button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="politics">View More</button></div>');
+            try {
+                $('#politicsNewsSection').find('.container > .row:first').append('<div class="col-md-2"><h6>' + data.response.results[i]['webTitle'] + '</h6>' + '<img src="' + data.response.results[i]['blocks']['main']['elements'][0]['assets'][0]['file'] + '" class="img-fluid">' +
+                    '<button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="politics">View More</button></div>');
+            }
+            catch (err) {
+                $('#politicsNewsSection').find('.container > .row:first').append('<div class="col-md-2"><h6>' + data.response.results[i]['webTitle'] + '</h6>' + '<img src="images/image_unavailable.png" class="img-fluid">' +
+                    '<button type="button" class="full-article btn btn-success" data-target="' + i + '" data-category="politics">View More</button></div>');
+            }
         }
 
-        for (var a = 0; a < data.articles.results.length; a++) {
+        for (var a = 0; a < data.response.results.length; a++) {
             searchData.push({
-                "label": data.articles.results[a]["title"],
+                "label": data.response.results[a]["webTitle"],
                 "category": "politics",
                 "index": a
             });
